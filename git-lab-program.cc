f85d48b8f1ca50764f1b39da9ae66b2bd636cb27
Seas-dev <seastedt.ej@gmail.com>
2021-09-03 17:55:44 -0400

    first commit after cloning git-lab-2

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jon Seastedt
+ *        Date: 9/3/2021
+ * Description: Lab 2 git-lab-2 cloning exercise
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
